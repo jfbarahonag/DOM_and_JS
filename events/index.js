980eecd96790619dfa1a304a9bf4a0fb9f12a9ee
jfbarahonag <felipe.barahona@titoma.com>
2021-04-13 01:04:31 -0500

    Second example of event delegation was implemented

--- events/index.js
+++ events/index.js
@@ -22,6 +22,17 @@ const showAlert = (evt) => {
     alert(`Logging ${username.value}`)
 }
 
+const createElement = (type, className, fillContent = false, content = 'Element') => {
+    const node = document.createElement(type)
+    node.className = className
+    if (!fillContent) {
+        return node
+    }
+    node.textContent = content
+
+    return node
+}
+
 submit.addEventListener('click', showAlert)
 footer.addEventListener('click', (evt) => console.log(`click ${evt.currentTarget.nodeName}`))
 div.addEventListener('click', (evt) => console.log(`click ${evt.currentTarget.nodeName}`))
@@ -29,6 +40,16 @@ button.addEventListener('click', (evt) => {
     evt.stopPropagation();
     console.log(`click ${evt.currentTarget.nodeName}`)
     console.log(github);
+    // Add a new element
+    const node = createElement('button', 'jajejijo', true)
+    button.append(node)
+    node.addEventListener('click', (evt) => {
+        evt.stopPropagation()
+        if (evt.target.classList.contains('jajejijo')) {
+            console.log('hey');
+        }
+    })
+    button.disabled = true
 })
 
 /**
@@ -36,13 +57,12 @@ button.addEventListener('click', (evt) => {
  * Only works with existing elements in the HTML 
  */
 let eventDelegationFunction = (evt) => {
-    console.log(evt.target.nodeName);
     if(evt.target.nodeName === 'DIV') {
         window.alert('hello')
     } else if (evt.target.nodeName === 'FOOTER') {
         window.alert('hola')
     } else if (evt.target.nodeName === 'BUTTON') {
-        // This wont work due to stop propagation used in Line:29
+        // This wont work due to stop propagation used in Line:40
         window.alert('bonjour')
     } else {
         window.alert('Ni hao')
