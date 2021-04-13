a167e90265e947741f414138d0a2865b524521fc
jfbarahonag <felipe.barahona@titoma.com>
2021-04-13 00:05:26 -0500

    First example of event delegation was implemented

--- events/index.js
+++ events/index.js
@@ -30,3 +30,23 @@ button.addEventListener('click', (evt) => {
     console.log(`click ${evt.currentTarget.nodeName}`)
     console.log(github);
 })
+
+/**
+ * 
+ * Only works with existing elements in the HTML 
+ */
+let eventDelegationFunction = (evt) => {
+    console.log(evt.target.nodeName);
+    if(evt.target.nodeName === 'DIV') {
+        window.alert('hello')
+    } else if (evt.target.nodeName === 'FOOTER') {
+        window.alert('hola')
+    } else if (evt.target.nodeName === 'BUTTON') {
+        // This wont work due to stop propagation used in Line:29
+        window.alert('bonjour')
+    } else {
+        window.alert('Ni hao')
+    }
+}
+
+footer.addEventListener('click', eventDelegationFunction)
